240d1e65fe7d2f462f7e696474c219622b8f6643
unknown <zhra5825@gmail.com>
2022-06-21 06:08:18 +0300

    f

---
+++ src/App.js
@@ -0,0 +1,42 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react';
+import './App.css';
+
+
+
+
+function App(){
+
+
+
+  return (
+    <div className="App">
+        <h1>cats </h1>
+   
+    <img src= "/pic/cat1.png" className="img"/>
+    <h1>name :white cat </h1>
+    <h2>age 2</h2>
+    <hr></hr>
+    <img src= "/pic/cat2.png" className="img"/>
+    <h1>name :angry cat </h1>
+    <h2>age 3</h2>
+    <hr></hr>
+    <img src= "/pic/cat3.png" className="img"/>
+    <h1>name :cute cat </h1>
+    <h2>age 2</h2>
+    <hr></hr>
+
+    
+   
+
+
+
+
+
+    </div>    
+    
+  );
+  }
+
+
+export default App;
